4b30c94bf1129b9b72b53933966b6e7279e342be
Paulo Pinheiro <pauloprap57@gmail.com>
2022-05-05 15:42:54 -0300

    update

--- web/src/components/Widget.tsx
+++ web/src/components/Widget.tsx
@@ -3,14 +3,22 @@ import { ChatTeardropDots } from "phosphor-react";
 
 export function Widget() {
   return (
-    <Popover className="absolute bottom-6 right-6">
+    <Popover className={"absolute bottom-6 right-6"}>
       <Popover.Panel>Ola Mundo</Popover.Panel>
 
-      <Popover.Button className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
-        <ChatTeardropDots className="w-6 h-6" />
+      <Popover.Button
+        className={
+          "bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group"
+        }
+      >
+        <ChatTeardropDots className={"w-6 h-6"} />
 
-        <span className="max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
-          <span className="pl-2"></span>
+        <span
+          className={
+            "max-w-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear"
+          }
+        >
+          <span className={"pl-2"}></span>
           Feedback
         </span>
       </Popover.Button>
